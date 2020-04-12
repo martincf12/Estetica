2e045bba9a040e0c2a12d868b5a569ec9e572d9d
Martin_DvC <martin.cf1289@gmail.com>
2020-04-12 00:48:12 -0300

    ultimos cambios de ui

--- src/controlador/Controlador.java
+++ src/controlador/Controlador.java
@@ -1,5 +1,28 @@
 package controlador;
 
+import javafx.scene.layout.Pane;
+
 public class Controlador {
+
+    public Pane hojaDatosPersonales;
+    public Pane hojaAntecedentes;
+    public Pane hojaSemiologia;
+    public Pane hojaMedidas;
+
+    public void traerAdelanteDatosPersonales(){
+        hojaDatosPersonales.toFront();
+    }
+
+    public void traerAdelanteAntecedentes(){
+        hojaAntecedentes.toFront();
+    }
+
+    public void traerAdelanteSemiologia(){
+        hojaSemiologia.toFront();
+    }
+
+    public void traerAdelanteMedidas(){
+        hojaMedidas.toFront();
+    }
 
 }
